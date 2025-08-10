d230bd6ebfb3161bdb2d923ecc3169a6f7b2be6e
Adam Byrne <adam.r.byrne@gmail.com>
2025-08-10 21:49:07 +0100

    hmr: remove alerts, added my own clipboard mgr

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,14 @@
-local alerts = require("alerts")
 local modes = require("modes")
 local apps = require("apps")
 local windows = require("windows")
-
--- Setup alert styles
-alerts.setup()
+local clipboard = require("clipboard")
 
 -- Setup modes with app and window bindings
 modes.setup(apps.bindKeys, windows.bindKeys)
+clipboard.setup()
 
 -- Reload Config
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
 	hs.reload()
 end)
+
